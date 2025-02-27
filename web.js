37903412d735fd80424cee325556e38ecd01d98c
farooqzain <farooqzain729@gmail.com>
2025-02-27 12:22:18 +0500

    Add files via upload

---
+++ web.js
@@ -0,0 +1,10 @@
+// Initialize Particles.js
+particlesJS("particles-js", {
+    particles: {
+        number: { value: 80 },
+        size: { value: 3 },
+        move: { speed: 2 },
+        line_linked: { enable: true, color: "#ffffff" },
+        color: { value: "#ffffff" }
+    }
+});
